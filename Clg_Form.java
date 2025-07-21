b0c343e89e9c5c7804eb1b931be6dc87f91bc0f3
sowmya492 <divyasrisowmya8985@gmail.com>
2025-07-21 10:59:02 +0530

    modified : print statements

---
+++ Clg_Form.java
@@ -0,0 +1,97 @@
+public class Clg_Form {
+
+    public static void main(String[] args) {
+        char ifStudent1stYear = 'F';
+        char ifStudentTransferee = 'T';
+        char ifStudentReturnee = 'R';
+        String Last_name ="Vathsav";
+        String First_name ="sri";
+        String Middle_name ="Pranav";
+        //complete address
+        String DOB = "03/06/2003";
+        String BirthPlace = "Manila";
+        //citizenShip
+        String ifCitizenShipFilipino = "Filipino";
+        String ifCitizenShipOthers = "Other";
+        //civilStatus
+        String ifCivilStatusSingle = "Single";
+        String ifCivilStatusMarried = "Married";
+        //gender
+        char ifGenderFemale = 'F';
+        char ifGenderMale = 'M';
+        char ifGenderOthers ='O';
+        //contact
+        long ContactNumber = 1234567890;
+        //religion
+        String Religion = "Hindhu";
+        //NameOfSpouse
+        String NameOfSpouse ="Sravya";
+        //Contact of spouse
+        long ContactNumOfSpouse = 9567892197l;
+        //Father details
+        //note : indicate + after the name if deceased
+        String FatherName = "Sri Vathsav";
+        String FOccupation ="Teacher";
+        long FContact = 7890654321l;
+        //Mother details
+        String MotherName = "Renuka Vathsav";
+        String MOccupation ="House Maker";
+        long MContact = 7890654332l;
+        //Gaudian's details
+        String GaurdianName = "Deepak Vathsav";
+        String GaurdiansAdress = "SomeColony,SomeCity,56789";
+        long Contact = 7899991332l;
+        //educational details
+        String SHS = "Senior HS";
+        String SHSstrand = "Science & Humanities";
+        Short yearPassed = 2024;
+        float genAvg = 75.9f;
+        //For Transferee / TECH-VOC COURSE graduate/2nd course
+        String LastSchoolAttended = "somexyz";
+        String course ="xyz";
+        short LastSchoolYear = 2022;
+        
+        boolean ifALSpasser = true;
+        boolean ifALSfail = false;
+        Short AlsYear = 2023;
+        // is Your Family 4ps benificiary?
+        boolean if4psbenificiary = true;
+        boolean ifnot4psbenificiary = false;
+        long IdNumOf4ps = 23145678901234567l; 
+        boolean anyDisabilityORpwd = true;
+        String TypeOfDisability ="ertyu";
+        boolean ifNOt = false;
+
+
+
+        System.out.println("if student 1st year" + ifStudent1stYear);
+        System.out.println("ifStudent Transferee" + ifStudentTransferee);
+        System.out.println("ifStudent Returnee" + ifStudentReturnee);
+        System.out.println("Last name" + Last_name);
+        System.out.println("First name" + First_name);
+        System.out.println("MIddle name" + Middle_name);
+        System.out.println("Dob" + DOB);
+        System.out.println("Birth place" + BirthPlace);
+        System.out.println("citizen ship" + ifCitizenShipFilipino);
+        System.out.println("citizenship others" + ifCitizenShipOthers);
+        System.out.println("if single" + ifCivilStatusSingle);
+        System.out.println("if married" + ifCivilStatusMarried);
+        System.out.println("gender" + ifGenderFemale);
+        System.out.println("gender" + ifGenderMale);
+        System.out.println("gender" + ifGenderOthers);
+        
+
+
+
+
+
+
+        
+
+
+        
+
+
+}
+
+}
