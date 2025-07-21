5fbe428aa4d6310b4452b7a1c8a51587465a8840
sowmya492 <divyasrisowmya8985@gmail.com>
2025-07-21 13:24:30 +0530

    form details

--- Clg_Form.java
+++ Clg_Form.java
@@ -60,30 +60,80 @@ public class Clg_Form {
         long IdNumOf4ps = 23145678901234567l; 
         boolean anyDisabilityORpwd = true;
         String TypeOfDisability ="ertyu";
-        boolean ifNOt = false;
+        boolean ifNOtDisable = false;
+        
+        
+        
+        System.out.println("-------------College Admission Form ---------------");
 
 
+        System.out.println("if student 1st year : " + ifStudent1stYear);
+        System.out.println("ifStudent Transferee : " + ifStudentTransferee);
+        System.out.println("ifStudent Returnee : " + ifStudentReturnee);
 
-        System.out.println("if student 1st year" + ifStudent1stYear);
-        System.out.println("ifStudent Transferee" + ifStudentTransferee);
-        System.out.println("ifStudent Returnee" + ifStudentReturnee);
-        System.out.println("Last name" + Last_name);
-        System.out.println("First name" + First_name);
-        System.out.println("MIddle name" + Middle_name);
-        System.out.println("Dob" + DOB);
-        System.out.println("Birth place" + BirthPlace);
-        System.out.println("citizen ship" + ifCitizenShipFilipino);
-        System.out.println("citizenship others" + ifCitizenShipOthers);
-        System.out.println("if single" + ifCivilStatusSingle);
-        System.out.println("if married" + ifCivilStatusMarried);
-        System.out.println("gender" + ifGenderFemale);
-        System.out.println("gender" + ifGenderMale);
-        System.out.println("gender" + ifGenderOthers);
-        System.out.println("Student contact" + ContactNumber);
-        System.out.println("Religion");
+        System.out.println("--------- Student Details ---------");
+        System.out.println("Last name : " + Last_name);
+        System.out.println("First name : " + First_name);
+        System.out.println("MIddle name : " + Middle_name);
+        System.out.println("Dob : " + DOB);
+        System.out.println("Birth place : " + BirthPlace);
+        System.out.println("citizen ship : " + ifCitizenShipFilipino);
+        System.out.println("citizenship others : " + ifCitizenShipOthers);
+        System.out.println("if single : " + ifCivilStatusSingle);
+        System.out.println("if married : " + ifCivilStatusMarried);
+        System.out.println("gender : " + ifGenderFemale);
+        System.out.println("gender : " + ifGenderMale);
+        System.out.println("gender : " + ifGenderOthers);
+        System.out.println("Student contact : " + ContactNumber);
+        System.out.println("Religion : " + Religion );
+        
+        System.out.println("=== If Married Fill Spouse Details ===");
+        
+        System.out.println("Name : " + NameOfSpouse);
+        System.out.println("Contact:" + ContactNumOfSpouse);
+        
+        System.out.println("=== Father's Details ===");
+        System.out.println("Father's Name : " + FatherName);
+        System.out.println("Occupation : " + FOccupation);
+        System.out.println("Contact : " + FContact);
+
+        System.out.println("==== Mother's Details ====");
+        System.out.println("Name : " + MotherName);
+        System.out.println("Occupation : " + MOccupation);
+        System.out.println("Contact : " + MContact);
+
+        
+        System.out.println("Gaudian details");
+        System.out.println("Name :" + GaurdianName);
+        System.out.println("Address : " + GaurdiansAdress);
+        System.out.println("Contact : " + Contact);
+
+        System.out.println("=== Education Details ===");
+        System.out.println("SHS Name : " + SHS);
+        System.out.println("SHS strad name : " + SHSstrand);
+        System.out.println("Year of pass : " + yearPassed);
+        System.out.println("Gen.Avg : " + genAvg);
+        System.out.println("Last School Attended : " +LastSchoolAttended );
+        System.out.println("Course name : " + course);
+        System.out.println("Year of pass : " +LastSchoolYear);
 
 
+        System.out.println("=== ALS details ===");
+        System.out.println("ALS Passed : " + ifALSpasser);
+        System.out.println("If ALS failed : " + ifALSfail);
+        System.out.println("Year of pass : " + AlsYear);
 
+
+        System.out.println("==== 4ps Benificiary ====");
+        System.out.println("4ps benificiary : " + if4psbenificiary);
+        System.out.println("Id Num : " + IdNumOf4ps);
+        System.out.println("if no benificiery : " + ifnot4psbenificiary);
+
+
+        System.out.println("==== Disability checking ====");
+        System.out.println("is Student disable : " + anyDisabilityORpwd);
+        System.out.println("Disability Type : " + TypeOfDisability);
+        System.out.println("If Not Disable : " + ifNOtDisable);
 
 
 
